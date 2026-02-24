0c4a71ae62bac4b1feb234c1ca572e676790bba8
Jaap Goorhuis <info@crewa.nl>
2026-02-24 19:12:28 +0100

    test

--- resources/views/livewire/frontend/pagebuilder/pagebuilder.blade.php
+++ resources/views/livewire/frontend/pagebuilder/pagebuilder.blade.php
@@ -140,7 +140,7 @@
                                                                         : "bottom: $captionBottom;";
                                                                 @endphp
 
-                                                                <div class="carousel-caption d-flex align-items-center justify-content-center text-center">
+                                                                <div class="carousel-caption align-items-center justify-content-center text-center">
                                                                     {!! $slide['text'] !!}
                                                                 </div>
                                                             </div>
