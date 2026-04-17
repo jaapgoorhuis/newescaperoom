18973fed22e145a74031dffb0931c8a12ba56c35
Jaap Goorhuis <info@crewa.nl>
2026-04-17 13:09:10 +0200

    reserveren

--- resources/views/livewire/frontend/pagebuilder/blocks/reserveren.blade.php
+++ resources/views/livewire/frontend/pagebuilder/blocks/reserveren.blade.php
@@ -16,7 +16,7 @@ function loadBookingWidget() {
 
     // script laden
     var s = document.createElement("script");
-    s.src = "https://widget.onlineafspraken.nl/consumer/booking/book/key/alah21oooa98-alaz00/l/a3216/ln/nl/t/8090ec/f/110d0111/o/theme:advanced,dp:modern/c/00aeef,7dc473,fff,f16522,56ac4a,111,0,fff/at/0/rs/0/pp/0/lp/1/ls/1/og/2/op/2/gp/0/gtm_tag/GTM-N643DK8M/exclude/mobiledetect/output/js";
+    s.src = "https://widget.onlineafspraken.nl/consumer/booking/book/key/alah21oooa98-alaz00/l/a3216/ln/nl/t/80c0ec/f/110d0111/o/theme:advanced,dp:modern/c/00aeef,7dc473,fff,f16522,56ac4a,111,0,fff/at/383844,358843,562857/rs/0/pp/0/lp/1/ls/1/og/2/op/2/gp/0/gtm_tag/GTM-N643DK8M/exclude/mobiledetect/output/js";
     s.id = "oa_script_loaded";
     document.body.appendChild(s);
 }
